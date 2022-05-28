251561b24f700e3495646b00351b251aa46d9f4a
BaraahAbusara <baraaabusara@gmail.com>
2022-05-29 01:29:52 +0300

    lab 32 : data is not going to the database yet and having problem with the tasks list which is not appearing in the beginning unless I go to another activity and come back

--- app/src/main/java/com/example/taskmaster/AddTaskAct.java
+++ app/src/main/java/com/example/taskmaster/AddTaskAct.java
@@ -1,6 +1,9 @@
 package com.example.taskmaster;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -8,19 +11,31 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.api.aws.AWSApiPlugin;
+import com.amplifyframework.api.graphql.model.ModelMutation;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.AWSDataStorePlugin;
 
 public class AddTaskAct extends AppCompatActivity {
 
     private static final String TAG = "test";
 
+    private Handler handler ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
+        configureAmplify();
+
+
+
         //------------------------------------spinner
         final String[] mState = new String[]{"New", "Assigned", "In progress", "complete"};
 
@@ -43,10 +58,10 @@ public class AddTaskAct extends AppCompatActivity {
             }
         });
 
-        //------------------------------------------
+        //------------------------------------------saving the task
         Button button = findViewById(R.id.button);
         button.setOnClickListener(view -> {
-
+        // take the data
             EditText titleField = findViewById(R.id.title);
             String title = titleField.getText().toString();
 
@@ -55,14 +70,74 @@ public class AddTaskAct extends AppCompatActivity {
 
             String state = taskStateSelector.getSelectedItem().toString();
 
-            Task newTask = new Task(title,body,state);
-            Long newTaskId = AppDatabase.getInstance(getApplicationContext()).userDao().insertTask(newTask);
 
+            // create a task
+            com.amplifyframework.datastore.generated.model.Task newTask = com.amplifyframework.datastore.generated.model.Task.builder()
+                    .title(title)
+                    .body(body)
+                    .status(state)
+                    .build();
 
-            Toast.makeText(this, "Task Added : "+newTask.getBody(), Toast.LENGTH_SHORT).show();
+            // save the task locally
+            Amplify.DataStore.save(
+                    newTask,
+                    success -> {
+                        handler = new Handler(Looper.getMainLooper(),msg -> {
+                            Toast.makeText(getApplicationContext(), "Task Added : "+newTask.getTitle(), Toast.LENGTH_SHORT).show();
+                            return true;
+                        });
+
+                    } ,
+                    fail->{
+                        handler = new Handler(Looper.getMainLooper(),msg -> {
+                            Toast.makeText(getApplicationContext(), "Task Failed to be Added", Toast.LENGTH_SHORT).show();
+                            return true;
+                        });
+                    }
+
+            );
+
             Log.i(TAG, "onCreate: adding done -> "+newTask.getId());
 
         });
+
+        // saves to the backend
+//            Amplify.API.mutate(ModelMutation.create(newTask), success -> {}, fail -> {});
+
+        Amplify.DataStore.observe(com.amplifyframework.datastore.generated.model.Task.class,
+                started -> {
+                    Log.i(TAG, "onCreate: observation began");
+                },
+                change -> {
+                    Log.i(TAG, change.item().toString());
+
+                    Bundle bundle = new Bundle();
+                    bundle.putString("data",change.item().toString());
+                    Message message = new Message();
+                    message.setData(bundle);
+
+                    handler.sendMessage(message);
+                },
+                failure -> {
+                    Log.e(TAG, "onCreate: Observation failed");
+                },
+                () -> {
+                    Log.i(TAG, "onCreate: Observation complete");
+                }
+        );
+
+    }
+
+    private void configureAmplify() {
+        try {
+            Amplify.addPlugin(new AWSApiPlugin());
+            Amplify.addPlugin(new AWSDataStorePlugin());
+            Amplify.configure(getApplicationContext());
+
+            Log.i(TAG, "Initialized Amplify");
+        } catch (AmplifyException e) {
+            Log.e(TAG, "Could not initialize Amplify", e);
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/taskmaster/Group.java
@@ -0,0 +1,12 @@
+package com.example.taskmaster;
+
+import java.util.List;
+
+public class Group {
+
+    List<Task> groupTasks;
+
+    public Group() {
+    }
+
+}
